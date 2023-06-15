497113147d24061cb6799af47da4a827aa8720aa
SahilSingh021 <sahilsingh.preet21@gmail.com>
2023-06-15 11:53:18 +0100

    Experince Section now has a slideshow and all is working.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,53 @@
 
-function scrollToSecondary() {
-    const secondarySection = document.getElementById('secondary');
-    secondarySection.scrollIntoView({ behavior: 'smooth' });
+function scrollToElementId(elementId) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+function scrollToElement(element) {
+    element.scrollIntoView({ behavior: "smooth" });
+}
+
+function openLinkInNewTab(event, url) {
+  event.preventDefault();
+  window.open(url, '_blank');
+}
+
+let currentSectionIndex = 1;
+
+document.addEventListener('DOMContentLoaded', function(){
+  if (currentSectionIndex !== 1){
+    return;
   }
-  
-  function openLinkInNewTab(event, url) {
-    event.preventDefault();
-    window.open(url, '_blank');
+
+  var leftArrowElement = document.getElementById('left-arrow');
+  leftArrowElement.style.display = 'none';
+});
+
+const leftArrow = document.getElementById('left-arrow');
+const rightArrow = document.getElementById('right-arrow');
+
+leftArrow.addEventListener('click', function(){
+  if (currentSectionIndex > 1){
+    currentSectionIndex--;
+    rightArrow.style.display = 'block';
+    scrollToElementId(`wraped-section-${currentSectionIndex}`);
   }
+  if (currentSectionIndex === 1){
+    leftArrow.style.display = 'none';
+  }
+});
+
+let maxNumberOfSection = 2/*Number of Slides*/;
+rightArrow.addEventListener('click', function(){
+  if (currentSectionIndex < maxNumberOfSection){
+    currentSectionIndex++;
+    leftArrow.style.display = 'block';
+    scrollToElementId(`wraped-section-${currentSectionIndex}`);
+  }
+  if (currentSectionIndex === maxNumberOfSection){
+    rightArrow.style.display = 'none';
+  }
+});
